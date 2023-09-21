86b0980f7324ca454cc8f6e2bd662d620f69ecf7
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-09-21 17:46:44 +0200

    Update __init__.py

--- rss007d0675444aa13fc/__init__.py
+++ rss007d0675444aa13fc/__init__.py
@@ -466,8 +466,12 @@ async def query(parameters: dict) -> AsyncGenerator[Item, None]:
             logging.info(f"[RSS newsfeed]\tDate = {created_at_formatted}")
             logging.info(f"[RSS newsfeed]\tTitle = {article.title}")
             logging.info(f"[RSS newsfeed]\tArticle content = {str(article.content)}")        
+
+            processed_content = str(article.content).replace("\n", " .").replace("\r", " .").replace("\t", " ")
+            # replace also quotes and double quotes
+            processed_content = processed_content.replace("\"", " ").replace("\'", " ")
             new_item = Item(
-                content=Content(str(article.content)),
+                content=Content(str(processed_content)),
                 # author=Author(str(source_domain)),
                 created_at=CreatedAt(created_at_formatted_capped),
                 title=Title(article.title),
