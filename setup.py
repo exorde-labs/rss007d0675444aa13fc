3107df0207b2df10cc63de028988d5f5ffa3e681
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-11-02 12:31:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="rss007d0675444aa13fc",
-    version="0.2.14",
+    version="0.2.15",
     packages=find_packages(),
     install_requires=[
         "exorde_data",
         "aiohttp",
         "tldextract>=3.1.0",
         "feedparser>=6.0.8",
-        "newspaper3k==0.2.8",
+        "newspaper3k>=0.2.8",
         "pytz>=2023.3",
         "python_dateutil>=2.8.2"
     ],
