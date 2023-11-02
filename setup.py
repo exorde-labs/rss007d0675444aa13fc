51af769f9210e75fcff08370f34bdf6fc15ab337
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-11-02 09:56:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         "aiohttp",
         "tldextract>=3.1.0",
         "feedparser>=6.0.8",
-        "newspaper3k>=0.2.8",
+        "newspaper3k==0.2.8",
         "pytz>=2023.3",
         "python_dateutil>=2.8.2"
     ],
